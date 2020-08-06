0664f34560288d3eae17ba917a340aa8147e6318
Richard Moss <richard.moss@cyotek.com>
2020-08-06 08:14:18 +0100

    WIP for basic simulation serialisation

---
+++ src/Encoding.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using Enc = System.Text.Encoding;
+
+namespace Cyotek.Demo.ChemotaxisSimulation
+{
+  public static class Encoding
+  {
+    #region Private Fields
+
+    private static readonly Enc _utf8bomless = new UTF8Encoding(false);
+
+    #endregion Private Fields
+
+    #region Public Properties
+
+    public static Enc UTF8NoIdentifier
+    {
+      get { return _utf8bomless; }
+    }
+
+    #endregion Public Properties
+  }
+}
---
+++ tests/SimulationSerializerTests.cs
@@ -0,0 +1,161 @@
+﻿using Cyotek.Demo.ChemotaxisSimulation;
+using NUnit.Framework;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace Cyotek.ChemotaxisSimulation.Tests
+{
+  [TestFixture]
+  public class SimulationSerializerTests
+  {
+    #region Private Properties
+
+    private string DataPath
+    {
+      get { return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data"); }
+    }
+
+    #endregion Private Properties
+
+    #region Public Methods
+
+    [Test]
+    public void SaveAdvancedTest()
+    {
+      // arrange
+      SimulationSerializer target;
+      Simulation simulation;
+      string expected;
+      string actual;
+      StringWriter writer;
+
+      simulation = this.CreateDemonstrationSimulation();
+
+      expected = File.ReadAllText(this.GetDataFileName("advanced.sim"));
+
+      target = new SimulationSerializer();
+
+      writer = new StringWriter();
+
+      // act
+      target.Save(writer, simulation);
+
+      // assert
+      actual = writer.ToString();
+      //File.WriteAllText(@"D:\Checkout\trunk\cyotek\source\demo\ChemotaxisSimulation\tests\data\advanced.sim", actual);
+      Assert.AreEqual(expected, actual);
+    }
+
+    [Test]
+    public void SaveTest()
+    {
+      // arrange
+      SimulationSerializer target;
+      Simulation simulation;
+      string expected;
+      string actual;
+      StringWriter writer;
+
+      simulation = new Simulation
+      {
+      };
+
+      expected = File.ReadAllText(this.GetDataFileName("default.sim"));
+
+      target = new SimulationSerializer();
+
+      writer = new StringWriter();
+
+      // act
+      target.Save(writer, simulation);
+
+      // assert
+      actual = writer.ToString();
+      //File.WriteAllText(@"D:\Checkout\trunk\cyotek\source\demo\ChemotaxisSimulation\tests\data\default.sim", actual);
+      Assert.AreEqual(expected, actual);
+    }
+
+    #endregion Public Methods
+
+    #region Private Methods
+
+    private Simulation CreateDemonstrationSimulation()
+    {
+      Simulation simulation;
+
+      simulation = new Simulation
+      {
+        EnvironmentSeed = 20200806,
+        MovementSeed = 0736,
+        Size = new Size(100, 100),
+        RespawnAttractor = false
+      };
+
+      simulation.FoodSources.Add(new Chemoeffector
+      {
+        Position = new Point(25, 25),
+        Strength = 25
+      });
+
+      simulation.FoodSources.Add(new Chemoeffector
+      {
+        Position = new Point(75, 25),
+        Strength = 25
+      });
+
+      simulation.FoodSources.Add(new Chemoeffector
+      {
+        Position = new Point(25, 75),
+        Strength = 25
+      });
+
+      simulation.FoodSources.Add(new Chemoeffector
+      {
+        Position = new Point(75, 75),
+        Strength = 25
+      });
+
+      simulation.NoxiousSources.Add(new Chemoeffector
+      {
+        Position = new Point(50, 50),
+        Strength = 50
+      });
+
+      simulation.Strands.Add(new Strand
+      {
+        Position = new Point(25, 5),
+        Heading = Compass.South
+      });
+
+      simulation.Strands.Add(new Strand
+      {
+        Position = new Point(95, 25),
+        Heading = Compass.West
+      });
+
+      simulation.Strands.Add(new Strand
+      {
+        Position = new Point(75, 95),
+        Heading = Compass.North
+      });
+
+      simulation.Strands.Add(new Strand
+      {
+        Position = new Point(5, 75),
+        Heading = Compass.East
+      });
+
+      simulation.Run(500);
+
+      return simulation;
+    }
+
+    private string GetDataFileName(string baseName)
+    {
+      return Path.Combine(this.DataPath, baseName);
+    }
+
+    #endregion Private Methods
+  }
+}
